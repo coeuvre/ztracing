10a08e4b0be1cdebd0668f07d7f11431095ac6cd
Chi Wang <coeuvre@gmail.com>
2024-09-20 23:25:54 +0200

    Rename FrameBuffer to Bitmap.

--- src/ztracing_main.c
+++ src/ztracing_main.c
@@ -75,58 +75,58 @@ static Vec2I OS_GetWindowSize(OS_Window *window) {
   return result;
 }
 
-typedef struct OS_FrameBuffer OS_FrameBuffer;
-struct OS_FrameBuffer {
+typedef struct OS_Bitmap OS_Bitmap;
+struct OS_Bitmap {
   BITMAPINFO bi;
   Vec2I size;
   u32 *pixels;
 };
 
-static OS_FrameBuffer OS_CreateFrameBuffer(Vec2I size) {
-  OS_FrameBuffer fb = {0};
+static OS_Bitmap OS_CreateBitmap(Vec2I size) {
+  OS_Bitmap bitmap = {0};
   if (size.x > 0 && size.y > 0) {
-    fb.pixels = VirtualAlloc(0, size.x * size.y * 4, MEM_COMMIT | MEM_RESERVE,
-                             PAGE_READWRITE);
+    bitmap.pixels = VirtualAlloc(0, size.x * size.y * 4,
+                                 MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
   }
-  if (fb.pixels) {
-    fb.size = size;
-    fb.bi.bmiHeader.biSize = sizeof(fb.bi.bmiHeader);
-    fb.bi.bmiHeader.biWidth = size.x;
-    fb.bi.bmiHeader.biHeight = -size.y;
-    fb.bi.bmiHeader.biPlanes = 1;
-    fb.bi.bmiHeader.biBitCount = 32;
-    fb.bi.bmiHeader.biCompression = BI_RGB;
+  if (bitmap.pixels) {
+    bitmap.size = size;
+    bitmap.bi.bmiHeader.biSize = sizeof(bitmap.bi.bmiHeader);
+    bitmap.bi.bmiHeader.biWidth = size.x;
+    bitmap.bi.bmiHeader.biHeight = -size.y;
+    bitmap.bi.bmiHeader.biPlanes = 1;
+    bitmap.bi.bmiHeader.biBitCount = 32;
+    bitmap.bi.bmiHeader.biCompression = BI_RGB;
   }
-  return fb;
+  return bitmap;
 }
 
-static void OS_DestroyFrameBuffer(OS_FrameBuffer *fb) {
-  if (fb->pixels) {
-    VirtualFree(fb->pixels, 0, MEM_RELEASE);
-    ZeroMemory(fb, sizeof(*fb));
+static void OS_DestroyBitmap(OS_Bitmap *bitmap) {
+  if (bitmap->pixels) {
+    VirtualFree(bitmap->pixels, 0, MEM_RELEASE);
+    ZeroMemory(bitmap, sizeof(*bitmap));
   }
 }
 
-static void OS_ResizeFrameBuffer(OS_FrameBuffer *fb, Vec2I size) {
-  OS_DestroyFrameBuffer(fb);
-  *fb = OS_CreateFrameBuffer(size);
+static void OS_ResizeBitmap(OS_Bitmap *bitmap, Vec2I size) {
+  OS_DestroyBitmap(bitmap);
+  *bitmap = OS_CreateBitmap(size);
 }
 
-static void OS_CopyFrameBufferToWindow(OS_Window *window, OS_FrameBuffer *fb) {
-  if (fb->pixels) {
+static void OS_CopyBitmapToWindow(OS_Window *window, OS_Bitmap *bitmap) {
+  if (bitmap->pixels) {
     Vec2I size = OS_GetWindowSize(window);
     HDC hdc = GetDC(window->handle);
-    i32 width = Min(size.x, fb->size.x);
-    i32 height = Min(size.y, fb->size.y);
-    StretchDIBits(hdc, 0, 0, width, height, 0, 0, width, height, fb->pixels,
-                  &fb->bi, DIB_RGB_COLORS, SRCCOPY);
+    i32 width = Min(size.x, bitmap->size.x);
+    i32 height = Min(size.y, bitmap->size.y);
+    StretchDIBits(hdc, 0, 0, width, height, 0, 0, width, height, bitmap->pixels,
+                  &bitmap->bi, DIB_RGB_COLORS, SRCCOPY);
     ReleaseDC(window->handle, hdc);
   }
 }
 
-static void OS_DrawRect(OS_FrameBuffer *fb, Rect2 rect, u32 color) {
+static void OS_DrawRect(OS_Bitmap *bitmap, Rect2 rect, u32 color) {
   Vec2 fb_min = V2(0.0f, 0.0f);
-  Vec2 fb_max = Vec2FromVec2I(fb->size);
+  Vec2 fb_max = Vec2FromVec2I(bitmap->size);
   Vec2 min = ClampVec2(rect.min, fb_min, fb_max);
   Vec2 max = ClampVec2(rect.max, fb_min, fb_max);
   i32 min_x = RoundF32(min.x);
@@ -134,7 +134,7 @@ static void OS_DrawRect(OS_FrameBuffer *fb, Rect2 rect, u32 color) {
   i32 max_x = RoundF32(max.x);
   i32 max_y = RoundF32(max.y);
   for (i32 y = min_y; y < max_y; ++y) {
-    u32 *row = fb->pixels + fb->size.x * y;
+    u32 *row = bitmap->pixels + bitmap->size.x * y;
     for (i32 x = min_x; x < max_x; ++x) {
       row[x] = color;
     }
@@ -146,7 +146,7 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
   OS_Window window = OS_CreateWindow();
   OS_ShowWindow(&window);
 
-  OS_FrameBuffer fb = OS_CreateFrameBuffer(OS_GetWindowSize(&window));
+  OS_Bitmap bitmap = OS_CreateBitmap(OS_GetWindowSize(&window));
 
   b32 quit = 0;
   for (; !quit;) {
@@ -163,11 +163,12 @@ int WINAPI wWinMain(HINSTANCE instance, HINSTANCE prev_instance, PWSTR cmd_line,
       }
     }
     Vec2I window_size = OS_GetWindowSize(&window);
-    if (!EqualVec2I(fb.size, window_size)) {
-      OS_ResizeFrameBuffer(&fb, window_size);
+    if (!EqualVec2I(bitmap.size, window_size)) {
+      OS_ResizeBitmap(&bitmap, window_size);
     }
-    OS_DrawRect(&fb, R2(V2(0.0f, 0.0f), Vec2FromVec2I(fb.size)), 0x00FF00FF);
-    OS_CopyFrameBufferToWindow(&window, &fb);
+    OS_DrawRect(&bitmap, R2(V2(0.0f, 0.0f), Vec2FromVec2I(bitmap.size)),
+                0x00FF00FF);
+    OS_CopyBitmapToWindow(&window, &bitmap);
   }
 
   return 0;
