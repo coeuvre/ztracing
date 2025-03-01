1b34449bc69c6e0abf5a80664f8dbe6fe56507d4
Chi Wang <coeuvre@gmail.com>
2025-03-01 15:40:08 +0100

    Upgrade SDL to 3.2.4.

--- third_party/SDL3/windows/include/SDL3/SDL_revision.h
+++ third_party/SDL3/windows/include/SDL3/SDL_revision.h
@@ -48,9 +48,9 @@
  */
 #define SDL_REVISION "Some arbitrary string decided at SDL build time"
 #elif defined(SDL_VENDOR_INFO)
-#define SDL_REVISION "release-3.2.0-0-g535d80bad (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "release-3.2.4-0-gb5c3eab6b (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "release-3.2.0-0-g535d80bad"
+#define SDL_REVISION "release-3.2.4-0-gb5c3eab6b"
 #endif
 
 #endif /* SDL_revision_h_ */
